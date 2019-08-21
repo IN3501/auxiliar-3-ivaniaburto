f80a8c082210b9e224304219a095c01241953466
ivaniaburto <53836559+ivaniaburto@users.noreply.github.com>
2019-08-20 23:36:47 -0400

    desafío 2

---
+++ primavera19/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'home.html')
+
+def inputs(request):
+	return render(request, 'inputs.html')
+
+def formulario(request):
+	return render(request, 'formulario.html')
+		
